20709dfeb5a10d49f1eabeb474d03f6958723faa
josh-programmieren <87899799+josh-programmieren@users.noreply.github.com>
2022-08-17 14:11:58 +0200

    Removed firebase
    Now, the app uses a local database.

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from app.settings import SQL_ALCHEMY_DATABASE_URI
 def working_on_route():
     """This is a helper function to show that the app can't be used in the moment."""
     return "Working on it!"
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQL_ALCHEMY_DATABASE_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.add_url_rule('/', 'index', working_on_route)
     return app
--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,4 @@
 # This file provides the settings for the flask application.
 port=6500	# Port number for the flask application.
 debug=True	# Debug mode for the flask application.
-# End of snippet.
+SQL_ALCHEMY_DATABASE_URI = 'sqlite:///app.db'	# SQLAlchemy database URI.
